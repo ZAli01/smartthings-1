22ea8289fc89f070ad51bcb5de3a8372cc68a038
ZAli01 <25347305+ZAli01@users.noreply.github.com>
2019-06-14 11:40:06 -0400

    Update check-home.sh

--- smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
+++ smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
@@ -3,11 +3,17 @@
 Zahir=Away
 Farhanah=Away
 
-macaddress="`wl -i eth1 assoclist` `wl -i eth2 assoclist` `wl -i eth3 assoclist`"
+macaddress2=`wl -i eth2 assoclist`
 
 case "$macaddress" in
 	*CC:C0:79:FE:2F:96*)
 	Zahir=Home
+	;;
+esac
+
+case "$macaddress" in
+	*CC:C0:79:42:27:C6*)
+	Hadeyah=Home
 	;;
 esac
 
@@ -17,18 +23,49 @@ case "$macaddress" in
 	;;
 esac
 
+case "$macaddress2" in
+	*CC:C0:79:FE:2F:96*)
+	Zahir=Home
+	;;
+esac
+
+case "$macaddress2" in
+	*CC:C0:79:42:27:C6*)
+	Hadeyah=Home
+	;;
+esac
+
+case "$macaddress2" in
+	*A0:CC:2B:14:0E:57*)
+	Farhanah=Home
+	;;
+esac
+
 if [ "$Zahir" = Home ]
 then
 	if [ ! -f /jffs/scripts/users/zahir ]
 	then
-		touch /jffs/scripts/users/zahir
-		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
+	  touch /jffs/scripts/users/zahir
+  	  curl "https://graph-na02-useast1.api.smartthings.com/api/smartapps/installations/e9aeb481-42e1-4161-9ba7-5c976e3809fc/Phone/home?access_token=d6fa95f2-77ca-4f65-b972-3ca068d20a3a" -k
 	fi
 else
-	if [ -f /jffs/scripts/users/chad ]
+	if [ -f /jffs/scripts/users/zahir ]
 	then
-		rm -f /jffs/scripts/users/Zahir
-		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
+	  rm -f /jffs/scripts/users/Zahir
+	  curl "https://graph-na02-useast1.api.smartthings.com/api/smartapps/installations/e9aeb481-42e1-4161-9ba7-5c976e3809fc/Phone/away?access_token=d6fa95f2-77ca-4f65-b972-3ca068d20a3a" -k
+	fi
+fi
+
+if [ "$Hadeyah" = Home ]
+then
+	if [ ! -f /jffs/scripts/users/hadeyah ]
+	then
+		touch /jffs/scripts/users/hadeyah
+	fi
+else
+	if [ -f /jffs/scripts/users/hadeyah ]
+	then
+		rm -f /jffs/scripts/users/hadeyah
 	fi
 fi
 
@@ -37,12 +74,10 @@ then
 	if [ ! -f /jffs/scripts/users/farhanah ]
 	then
 		touch /jffs/scripts/users/farhanah
-		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
 	fi
 else
 	if [ -f /jffs/scripts/users/farhanah ]
 	then
 		rm -f /jffs/scripts/users/farhanah
-		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
 	fi
 fi
