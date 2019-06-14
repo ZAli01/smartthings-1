3d85703f01e0718b26f5cad4eb16a85c813c2faa
ZAli01 <25347305+ZAli01@users.noreply.github.com>
2019-06-14 10:27:26 -0400

    Update check-home.sh

--- smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
+++ smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
@@ -1,48 +1,48 @@
 #!/bin/sh
 
-Chad=Away
-Alanna=Away
+Zahir=Away
+Farhanah=Away
 
 macaddress="`wl -i eth1 assoclist` `wl -i eth2 assoclist` `wl -i eth3 assoclist`"
 
 case "$macaddress" in
-	*XX:XX:XX:XX:XX:XX*)
-	Chad=Home
+	*CC:C0:79:FE:2F:96*)
+	Zahir=Home
 	;;
 esac
 
 case "$macaddress" in
-	*XX:XX:XX:XX:XX:XX*)
-	Alanna=Home
+	*A0:CC:2B:14:0E:57*)
+	Farhanah=Home
 	;;
 esac
 
-if [ "$Chad" = Home ]
+if [ "$Zahir" = Home ]
 then
-	if [ ! -f /jffs/scripts/users/chad ]
+	if [ ! -f /jffs/scripts/users/zahir ]
 	then
-		touch /jffs/scripts/users/chad
+		touch /jffs/scripts/users/zahir
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
 	fi
 else
 	if [ -f /jffs/scripts/users/chad ]
 	then
-		rm -f /jffs/scripts/users/chad
+		rm -f /jffs/scripts/users/Zahir
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
 	fi
 fi
 
-if [ "$Alanna" = Home ]
+if [ "$farhanah" = Home ]
 then
-	if [ ! -f /jffs/scripts/users/alanna ]
+	if [ ! -f /jffs/scripts/users/farhanah ]
 	then
-		touch /jffs/scripts/users/alanna
+		touch /jffs/scripts/users/farhanah
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
 	fi
 else
-	if [ -f /jffs/scripts/users/alanna ]
+	if [ -f /jffs/scripts/users/farhanah ]
 	then
-		rm -f /jffs/scripts/users/alanna
+		rm -f /jffs/scripts/users/farhanah
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
 	fi
 fi
